d2ef970f0490f2d71d42655fb67a39f31cc36464
xuemeiwei <xuemeiwei1226@gmail.com>
2016-04-18 13:22:58 -0700

    code refine

--- application.py
+++ application.py
@@ -21,20 +21,18 @@ from settings import *
 EMAIL_SCOPE = endpoints.EMAIL_SCOPE
 API_EXPLORER_CLIENT_ID = endpoints.API_EXPLORER_CLIENT_ID
 NEW_GAME_REQUEST = endpoints.ResourceContainer(NewGameForm)
-GET_GAME_REQUEST = endpoints.ResourceContainer(
-        urlsafe_game_key=messages.StringField(1),)
-MAKE_MOVE_REQUEST = endpoints.ResourceContainer(
-    MakeMoveForm,
-    urlsafe_game_key=messages.StringField(1),)
+GET_GAME_REQUEST = endpoints.ResourceContainer(urlsafe_game_key=messages.StringField(1),)
+MAKE_MOVE_REQUEST = endpoints.ResourceContainer(MakeMoveForm,
+                                                                                    urlsafe_game_key=messages.StringField(1),)
 USER_REQUEST = endpoints.ResourceContainer(user_name=messages.StringField(1),
-                                           email=messages.StringField(2))
+                                                                           email=messages.StringField(2))
 GET_HIGHEST_SCORES_REQUEST = endpoints.ResourceContainer(results= messages.IntegerField(1))
-
 MEMCACHE_MOVES_REMAINING = 'MOVES_REMAINING'
 
 @endpoints.api(name='hangman', version='v1', audiences=[ANDROID_AUDIENCE],
     allowed_client_ids=[WEB_CLIENT_ID, API_EXPLORER_CLIENT_ID, ANDROID_CLIENT_ID, IOS_CLIENT_ID],
     scopes=[EMAIL_SCOPE])
+
 class HangmanApi(remote.Service):
     """Hangman API"""
     @endpoints.method(request_message=USER_REQUEST,
@@ -70,9 +68,9 @@ class HangmanApi(remote.Service):
             raise endpoints.NotFoundException(
                     'A user with that name does not exist!')
         try:
-	    game = Game.new_game(user.key,9)
-	except ValueError:
-	    raise endpoints.BadRequestException('error message')
+            game = Game.new_game(user.key,9)
+        except ValueError:
+            raise endpoints.BadRequestException('error message')
 
         # Use a task queue to update the average attempts remaining.
         # This operation is not needed to complete the creation of a new game
@@ -91,18 +89,19 @@ class HangmanApi(remote.Service):
         game = get_by_urlsafe(request.urlsafe_game_key, Game)
         if game.game_over:
             return game.to_form('Game already over!')
-        """code added to check letter has not already been guessed"""
+        """Check letter has not already been guessed"""
         if request.guess in game.letters_guessed:
-            raise endpoints.BadRequestException('repeated letter')
+            raise endpoints.BadRequestException('Repeated letter!')
         game.letters_guessed += request.guess
         hangmanprogress = ''
         failed = 0
+        """Check the letter you guesses is right or not"""
         for char in game.word_to_guess:
           if char not in game.letters_guessed:
            failed +=1
         if failed == 0:
           game.end_game(True)
-          return game.to_game_form('You win!')
+          return game.to_game_form('You won!')
         if request.guess in game.word_to_guess:
           msg = 'Your letter is in the secret word'
           game.history.append((request.guess," found"))
@@ -110,26 +109,14 @@ class HangmanApi(remote.Service):
         else:
           game.attempts_remaining -= 1
           game.history.append((request.guess,"not in word"))
-          """ append the move to the game history"""
-          msg = 'letter not there, another piece to the hangman'
+          """ Append the move to the game history"""
+          msg = 'The letter you guessed is not there!'
         if game.attempts_remaining ==0:
           game.end_game(False)
-          return game.to_form(msg + ' Game over!')
+          return game.to_game_form(msg + ' Game over!')
         else:
           game.put()
         return game.to_game_form(msg)
-
-    @endpoints.method(request_message=GET_GAME_REQUEST,
-                      response_message=StringMessage,
-                      path='game/{urlsafe_game_key}/history',
-                      name='get_game_history',
-                      http_method='GET')
-    def get_game_history(self, request):
-        """Returns a Game's history, namely all the moves"""
-        game = get_by_urlsafe(request.urlsafe_game_key, Game)
-        if not game:
-          raise endpoints.NotFoundException('Game not found!')
-        return StringMessage(message=str(game.history))
 
     @endpoints.method(request_message=GET_GAME_REQUEST,
                       response_message=GameForm,
@@ -137,27 +124,24 @@ class HangmanApi(remote.Service):
                       name='get_game',
                       http_method='GET')
     def get_game(self, request):
-        """Return the current game state."""
+        """Return the current state of a given name."""
         game = get_by_urlsafe(request.urlsafe_game_key, Game)
         if game:
             return game.to_game_form('Here is the requested game')
         else:
             raise endpoints.NotFoundException('Game not found!')
-    
 
-    @endpoints.method(request_message=USER_REQUEST,
-                      response_message=GameForms,
-                      path='user/games',
-                      name='get_user_games',
+    @endpoints.method(request_message=GET_GAME_REQUEST,
+                      response_message=StringMessage,
+                      path='game/{urlsafe_game_key}/history',
+                      name='get_game_history',
                       http_method='GET')
-    def get_user_games(self, request):
-        """Return all games of a user"""
-        user = User.query(User.name == request.user_name).get()
-        if not user:
-          raise endpoints.NotFoundException('User not found')
-        games = Game.query(Game.user == user.key).\
-        filter(Game.game_over == False)
-        return GameForms(items=[game.to_game_form("active game") for game in games])
+    def get_game_history(self, request):
+        """Returns a given game's history, namely all the moves"""
+        game = get_by_urlsafe(request.urlsafe_game_key, Game)
+        if not game:
+          raise endpoints.NotFoundException('Game not found!')
+        return StringMessage(message=str(game.history))
      
     @endpoints.method(request_message=GET_GAME_REQUEST,
                       response_message=StringMessage,
@@ -172,11 +156,9 @@ class HangmanApi(remote.Service):
         return StringMessage(message='Game id key:{} removed'.
         format(request.urlsafe_game_key))
       elif game and game.game_over:
-        raise endpoints.BadRequestException('Sorry cannot delete completed games')
+        raise endpoints.BadRequestException('Sorry! Completed games can not be deleted')
       else:
         raise endpoints.NotFoundException('No such game found') 
-
-
 
     @endpoints.method(response_message=ScoreForms,
                       path='scores',
@@ -192,9 +174,23 @@ class HangmanApi(remote.Service):
                       name='get_highest_scores',
                       http_method='GET')
     def get_highest_scores(self, request):
-        """Return scores graded out from highest to lowest"""
+        """Return the scores graded from highest to lowest"""
         Scores =Score.query(Score.won == True).order(Score.guesses).fetch(request.results)
         return ScoreForms(items=[score.to_score_form() for score in Scores])
+
+    @endpoints.method(request_message=USER_REQUEST,
+                      response_message=GameForms,
+                      path='user/games',
+                      name='get_user_games',
+                      http_method='GET')
+    def get_user_games(self, request):
+        """Return all the games of a given user"""
+        user = User.query(User.name == request.user_name).get()
+        if not user:
+          raise endpoints.NotFoundException('User not found')
+        games = Game.query(Game.user == user.key).\
+        filter(Game.game_over == False)
+        return GameForms(items=[game.to_game_form("active game") for game in games])
 
     @endpoints.method(response_message=UserForms,
                       path='user/rankings',
